763ff171e2e04bdd0b4cccc1d3f3917df8456608
castlevania7689@yahoo.com <castlevania7689@yahoo.com@dc052594-c8ac-11de-afa3-455a29e9879b>
2009-11-03 19:21:28 +0000

    DeSmuME 0.9.4 set-up to compile under devkitPPC. No changes to the source have been made at this point.

---
+++ build/FIFO.d
@@ -0,0 +1,37 @@
+FIFO.o: c:/projects/wii/desmumeWii/source/src/FIFO.cpp \
+  c:/projects/wii/desmumeWii/source/src/FIFO.h \
+  c:/projects/wii/desmumeWii/source/src/types.h \
+  c:/projects/wii/desmumeWii/source/src/armcpu.h \
+  c:/projects/wii/desmumeWii/source/src/bits.h \
+  c:/projects/wii/desmumeWii/source/src/MMU.h \
+  c:/projects/wii/desmumeWii/source/src/dscard.h \
+  c:/projects/wii/desmumeWii/source/src/mem.h \
+  c:/projects/wii/desmumeWii/source/src/ARM9.h \
+  c:/projects/wii/desmumeWii/source/src/PACKED.h \
+  c:/projects/wii/desmumeWii/source/src/PACKED_END.h \
+  c:/projects/wii/desmumeWii/source/src/mc.h \
+  c:/projects/wii/desmumeWii/source/src/debug.h
+
+c:/projects/wii/desmumeWii/source/src/FIFO.h:
+
+c:/projects/wii/desmumeWii/source/src/types.h:
+
+c:/projects/wii/desmumeWii/source/src/armcpu.h:
+
+c:/projects/wii/desmumeWii/source/src/bits.h:
+
+c:/projects/wii/desmumeWii/source/src/MMU.h:
+
+c:/projects/wii/desmumeWii/source/src/dscard.h:
+
+c:/projects/wii/desmumeWii/source/src/mem.h:
+
+c:/projects/wii/desmumeWii/source/src/ARM9.h:
+
+c:/projects/wii/desmumeWii/source/src/PACKED.h:
+
+c:/projects/wii/desmumeWii/source/src/PACKED_END.h:
+
+c:/projects/wii/desmumeWii/source/src/mc.h:
+
+c:/projects/wii/desmumeWii/source/src/debug.h:
