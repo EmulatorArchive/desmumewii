384159533bdcccfad24bc444af1fd60cc64583fe
castlevania7689@yahoo.com <castlevania7689@yahoo.com@dc052594-c8ac-11de-afa3-455a29e9879b>
2009-11-03 19:21:28 +0000

    DeSmuME 0.9.4 set-up to compile under devkitPPC. No changes to the source have been made at this point.

---
+++ trunk/build/Disassembler.d
@@ -0,0 +1,16 @@
+Disassembler.o: c:/projects/wii/desmumeWii/source/src/Disassembler.cpp \
+  c:/projects/wii/desmumeWii/source/src/Disassembler.h \
+  c:/projects/wii/desmumeWii/source/src/types.h \
+  c:/projects/wii/desmumeWii/source/src/bits.h \
+  c:/projects/wii/desmumeWii/source/src/instruction_tabdef.inc \
+  c:/projects/wii/desmumeWii/source/src/thumb_tabdef.inc
+
+c:/projects/wii/desmumeWii/source/src/Disassembler.h:
+
+c:/projects/wii/desmumeWii/source/src/types.h:
+
+c:/projects/wii/desmumeWii/source/src/bits.h:
+
+c:/projects/wii/desmumeWii/source/src/instruction_tabdef.inc:
+
+c:/projects/wii/desmumeWii/source/src/thumb_tabdef.inc:
