93b858177752663cc20d72344c5a28c9fd3ca156
firnis@ya.ru <firnis@ya.ru@dc052594-c8ac-11de-afa3-455a29e9879b>
2010-04-04 20:57:25 +0000

    Fix from official SVN r3404 and r3474.

--- source/src/Disassembler.cpp
+++ source/src/Disassembler.cpp
@@ -2735,7 +2735,7 @@ static char * OP_STRB_M_ROR_IMM_OFF_POSTIND(u32 adr, u32 i, char * txt)
 return txt;}
 
 //-----------------------LDRBT-------------------------------------
-
+#if 0
 static char * OP_LDRBT_P_IMM_OFF_POSTIND(u32 adr, u32 i, char * txt)
 {
      sprintf(txt, "LDRBT%s %s, [%s], #%X!", Condition[CONDITION(i)], Registre[REG_POS(i,12)], Registre[REG_POS(i,16)], (int)(i&0x7FF));
@@ -2837,7 +2837,7 @@ static char * OP_STRBT_M_ROR_IMM_OFF_POSTIND(u32 adr, u32 i, char * txt)
 {
      LDRSTR_ROR_IMM(STRBT, "-", "]", "");
 return txt;}
-
+#endif
 //---------------------LDM-----------------------------
 
 static char * OP_LDMIA(u32 adr, u32 i, char * txt)
