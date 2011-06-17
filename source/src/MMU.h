947acdd7e00413ff4bc6430a4ba75a8df2fdfc8f
dancinninjac <dancinninjac@dc052594-c8ac-11de-afa3-455a29e9879b>
2011-06-17 03:26:20 +0000

    Still updating 0.9.7. Argh. A lot of tedius little things in this update.  One or two of my own optimizations but nothing major.

--- source/src/MMU.h
+++ source/src/MMU.h
@@ -75,7 +75,9 @@ enum EDMADestinationUpdate
 	EDMADestinationUpdate_IncrementReload = 3,
 };
 
-
+//TODO
+//n.b. this may be a bad idea, for complex registers like the dma control register.
+//we need to know exactly what part was written to, instead of assuming all 32bits were written.
 class TRegister_32
 {
 public:
@@ -85,7 +87,6 @@ public:
 		if(size==32) write32(val);
 		else {
 			const u32 offset = adr&3;
-			//const u32 baseaddr = adr&~offset;
 			if(size==8) {
 				printf("WARNING! 8BIT DMA ACCESS\n"); 
 				u32 mask = 0xFF<<(offset<<3);
@@ -103,7 +104,6 @@ public:
 		if(size==32) return read32();
 		else {
 			const u32 offset = adr&3;
-			//const u32 baseaddr = adr&~offset;
 			if(size==8) { printf("WARNING! 8BIT DMA ACCESS\n"); return (read32()>>(offset<<3))&0xFF; }
 			else return (read32()>>(offset<<3))&0xFFFF;
 		}
